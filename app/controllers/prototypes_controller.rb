c7a638f437596187a9477b84085451dc1f3043c8
norifumi0129 <146012179+norifumi0129@users.noreply.github.com>
2023-11-14 11:25:10 +0900

    Update prototypes_controller.rb

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,7 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
-  def index
+  before_action :set_prototype, only: [:edit, :update, :destroy]
+  before_action :authorize_user!, only: [:edit, :update, :destroy]def index
     @prototypes = Prototype.with_attached_image.all
   end
 
@@ -47,5 +48,11 @@ class PrototypesController < ApplicationController
   private
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+  def authorize_user!
+    redirect_to root_path
   end
 end
