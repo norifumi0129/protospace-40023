1fa3c886b78592c8a651f21bc1a99825f8a3c4b1
norifumi0129 <146012179+norifumi0129@users.noreply.github.com>
2023-11-14 11:19:33 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
-    @user = Prototype.find(params[:id]).user
+    @user = Prototype.find(params[:id])
     @user = User.find(params[:id])
     @prototypes = @user.prototypes
   end
